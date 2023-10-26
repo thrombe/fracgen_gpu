a6b0fce18bceab76cf2cd30cb73944d821d5e82e
thrombe <yash.n.thombare620@gmail.com>
2023-10-26 22:35:27 +0530

    added nix shell

---
+++ shell.nix
@@ -0,0 +1,39 @@
+
+
+
+with import <nixpkgs> {};
+
+# - [Failed to initialize any backend! · emilk/egui · Discussion #1587 · GitHub](https://github.com/emilk/egui/discussions/1587)
+stdenv.mkDerivation rec {
+    name = "rust";
+    buildInputs = [
+      libxkbcommon
+      libGL
+
+      # WINIT_UNIX_BACKEND=wayland
+      wayland
+
+      # - [No appropiate adapter found](https://github.com/gfx-rs/wgpu/issues/3033)
+      # WINIT_UNIX_BACKEND=x11
+      xorg.libXcursor
+      xorg.libXrandr
+      xorg.libXi
+      xorg.libX11
+
+      # WGPU_BACKEND=vulkan
+      glslang
+      vulkan-headers
+      vulkan-loader
+      vulkan-validation-layers
+
+      # tools
+      # glxinfo # glxinfo
+      # wgpu-utils # wgpuinfo
+      # vulkan-tools # vulkaninfo
+    ];
+    LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+    # shellHook = ''
+    #     export OPENSSL_DIR="${openssl.dev}"
+    #     export OPENSSL_LIB_DIR="${openssl.out}/lib"
+    # '';
+}
